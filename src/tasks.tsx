624881a8627b8adebd695ef55b629aaa36548768
Erich Gubler <erichdongubler@gmail.com>
2022-09-17 21:47:30 -0400

    feat: add `Do hair` tasks

--- src/tasks.tsx
+++ src/tasks.tsx
@@ -6,6 +6,7 @@ import iconAbcMouse from "./assets/ABC Mouse.svg";
 import iconArt from "./assets/Art.svg";
 import iconBreakfast from "./assets/Breakfast.svg";
 import iconBrushTeeth from "./assets/Brush teeth.svg";
+import iconDoHair from "./assets/Do hair.svg";
 import iconGeography from "./assets/Geography.svg";
 import iconGetDressed from "./assets/Get dressed.svg";
 import iconMakeBed from "./assets/Make bed.svg";
@@ -38,6 +39,12 @@ export interface TasksSpec {
 }
 
 export const TASKS_SPECS: TasksSpec[] = [
+  {
+    task: {
+      svgUrl: iconDoHair,
+      label: "Do hair",
+    }
+  },
   {
     task: {
       svgUrl: iconAbcMouse,
